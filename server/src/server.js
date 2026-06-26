e886eaaeb2f3a8212eb13955884a3ff7ebf67b74
Vishant kohli <kumarvishant602@gmail.com>
2026-06-26 10:15:55 +0530

    Final resolved code

--- server/src/server.js
+++ server/src/server.js
@@ -24,7 +24,10 @@ connectDB();
 
 app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }));
 app.use(cors({
-  origin: true,
+  origin: [
+    process.env.CLIENT_URL,
+    "http://localhost:5173"
+  ],
   credentials: true
 }));
 app.use(express.json({ limit: '1mb' }));
